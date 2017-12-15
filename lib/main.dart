13a35a9cca4fb5b22b4e1f32b6d41d9ad0a9122e
German Saprykin <german.saprykin@it-nomads.com>
2017-12-15 21:54:52 +0800

    Step 1.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
 
 void main() => runApp(new MyApp());
 
@@ -17,16 +18,62 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class PopularMoviesWidget extends StatefulWidget {
-  @override
-  _PopularMoviesWidgetState createState() => new _PopularMoviesWidgetState();
-}
+class PopularMoviesWidget extends StatelessWidget {
+  final List<Movie> movies = Movie.allMovies();
 
-class _PopularMoviesWidgetState extends State<PopularMoviesWidget> {
   @override
   Widget build(BuildContext context) {
-    return new Center(
-      child: new Text("No content"),
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Popular"),
+      ),
+      body: new ListView.builder(
+        itemCount: movies.length,
+        itemBuilder: (context, i) {
+          final movie = movies[i];
+          return new ListTile(
+            title: new Text(movie.title),
+            subtitle: new Text(movie.description),
+            onTap: () => Navigator.of(context).push(_showDetails(movie)),
+          );
+        },
+      ),
     );
+  }
+
+  Route _showDetails(Movie movie) => new MaterialPageRoute<Null>(
+        builder: (BuildContext context) => new MovieDetails(movie: movie),
+      );
+}
+
+class MovieDetails extends StatelessWidget {
+  final Movie movie;
+
+  MovieDetails({@required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(movie.title),
+      ),
+      body: new Center(
+        child: new Text(movie.description),
+      ),
+    );
+  }
+}
+
+class Movie {
+  Movie({@required this.title, @required this.description});
+
+  final String title;
+  final String description;
+
+  static List<Movie> allMovies() {
+    final list = new List<int>.generate(10, (i) => i + 1);
+    return list
+        .map((i) => new Movie(title: "Title $i", description: "Description $i"))
+        .toList(growable: false);
   }
 }
